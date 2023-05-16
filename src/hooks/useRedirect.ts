0b4e9955fdb07aef33be2badd1fe08eb296e50e3
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2023-05-16 12:02:39 +0900

    feat: Add to login page redirection in my-drive

--- src/hooks/useRedirect.ts
+++ src/hooks/useRedirect.ts
@@ -3,17 +3,29 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 /**
- * 메인페이지 진입시 로그인여부 확인후 개인 드라이브 주소로 리다이렉트 여부 결정
+ * index 페이지와 my-drive 페이지에서 로그인 여부에 따라 리다이렉트 실행.
+ * index 페이지에서 로그인한 사용자는 my-drive로 리다이렉트, my-drive 페이지에서 로그인 안된사용자는 login 페이지로 리다이렉트.
+ * @param {boolean} toLogin - true일경우 로그인 안된 사용자는 login 페이지로 이동.
  */
-const useRedirect = () => {
+const useRedirect = (toLogin = false) => {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
   useEffect(() => {
     const check = async () => {
       const { data } = await checkLogin();
+
+      if (toLogin) {
+        if (!data) {
+          router.replace("/login");
+          return;
+        }
+        setLoading(false);
+      }
+
       if (data) {
         router.replace("/my-drive");
       }
+
       setLoading(false);
     };
     check();
--- src/pages/my-drive.tsx
+++ src/pages/my-drive.tsx
@@ -1,4 +1,15 @@
+import Spinner from "@/components/spinner/Spinner";
+import useRedirect from "@/hooks/useRedirect";
+
 const MyDrive = () => {
+  const { loading } = useRedirect(true);
+  if (loading) {
+    return (
+      <div>
+        <Spinner></Spinner>
+      </div>
+    );
+  }
   return <div>개인 드라이브 대쉬보드 페이지</div>;
 };
 
