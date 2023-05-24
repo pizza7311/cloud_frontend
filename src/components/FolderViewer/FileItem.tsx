8bb45b25e0b3bee1755f0213a4e1fbf26273a5f5
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2023-05-24 23:54:43 +0900

    refactor: Update FileItem thumbnail code

--- src/components/FolderViewer/FileItem.tsx
+++ src/components/FolderViewer/FileItem.tsx
@@ -17,26 +17,20 @@ type props = {
 
 const FileItem = ({ data, layout = "grid" }: props) => {
   if (layout === mode.grid) {
-    return data.thumbnail ? (
-      <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-        <img
-          className="rounded-t-lg"
-          src={data.thumbnail}
-          alt={data.fileName}
-        />
-        <div className="p-2">
-          <h5 className="text-lg text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            Card title
-          </h5>
-        </div>
-      </div>
-    ) : (
+    return (
       <div className="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 h-full">
-        <div className="w-full h-full">이미지 넣기</div>
-
+        {data.thumbnail ? (
+          <img
+            className="rounded-t-lg"
+            src={data.thumbnail}
+            alt={data.fileName}
+          />
+        ) : (
+          <div className="w-full h-full">이미지 넣기</div>
+        )}
         <div className="p-2">
           <h5 className="text-lg text-center font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            Card title
+            {data.fileName}
           </h5>
         </div>
       </div>
